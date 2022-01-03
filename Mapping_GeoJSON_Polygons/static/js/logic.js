c004b03ff39a4f8202a1dd9c6555026a93fd3e8e
alex-keigley <30334379+alex-keigley@users.noreply.github.com>
2022-01-03 12:56:15 -0600

    moving to 13.6.1

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Satellite street layer for background of map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Creating base layer that holds maps
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+}
+
+// Create the map object with center, zoom level, and default layer
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+})
+
+// Pass map layers into layer control
+L.control.layers(baseMaps).addTo(map)
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "../torontoNeighborhoods.json";
+
+// Grabbing geoJSON data
+d3.json(torontoHoods).then(function(data) {
+  console.log(data)
+  // Create GeoJSON layer with data
+  L.geoJSON(data).addTo(map)
+})
